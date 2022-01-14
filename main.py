005466e74b542222ea4a8ab3e650e75d8b67c147
kush5683 <32845063+kush5683@users.noreply.github.com>
2022-01-14 15:09:37 -0500

    added day to date

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from xmlrpc.client import Boolean
 from printColor import *
 import run_meliora
 from Meliora import Meliora
-import json
 
 # mode flag if 0 then text mode, if 1 then voice mode
 mode = -1
@@ -13,18 +12,15 @@ mode = -1
 
 def main():
     voice_mode = getInput(
-        "Would you like to run the program in voice mode? (y/n)", yellow).upper()
+        "Would you like to run in voice mode? (y/n)", yellow).upper()
 
     if voice_mode == "Y":
         red("Running in voice mode")
         mode = 1
-        # red("Starting voice mode...")
-        # do something
         pass
     else:
         red("Starting text mode...")
         mode = 0
-        # do something else
         pass
     MEL = Meliora(mode)
     run_meliora.setup(MEL)
